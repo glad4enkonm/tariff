0368c3391af715e235d90071df1d01758bb0f53d
Nikolai Gladchenko <Nikolai_Gladchenko@jabil.com>
2017-12-15 08:04:04 +0100

    Initial structure created

---
+++ Tariff.Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tariff.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
